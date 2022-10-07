d030f355a3491b2f228bde0805f647fcc9145617
starydv7 <pawanstar72926@gmail.com>
2022-10-08 00:21:29 +0530

    prototype

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ function greet(name) {
     console.log("hi" + " " + name);
 }
 greet();
+
+//set method
+const letters = new set["a", "b", "c"];
+
+//object map
+
+const fruits = new map(["apples", 500]);
+
+
+// ProtoType
+
+function Person(age, name, eyeColor) {
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.nationality = "Indian";
+console.log(name, age, nationality);
