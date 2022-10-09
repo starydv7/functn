77d344014d99b1b2d2fa7204a617278dbe60f742
starydv7 <pawanstar72926@gmail.com>
2022-10-10 01:38:11 +0530

    Contact manager

--- crash-react/src/components/Header.jsx
+++ crash-react/src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+      <div className='header'>
+          <div className='header-1'>
+              <h2>Contact Manager</h2>
+          </div>
+    </div>
   )
 }
 
