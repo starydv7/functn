5470ac2d2669d1fd341295c830ae794b16a2e3bc
starydv7 <pawanstar72926@gmail.com>
2022-10-09 23:59:06 +0530

    hoc

---
+++ Hoc.jsx
@@ -0,0 +1,19 @@
+function greaterThan(n) {
+  return m => m > n;
+}
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+// → true
+And we can have functions that change other functions.
+
+function noisy(f) {
+  return (...args) => {
+    console.log("calling with", args);
+    let result = f(...args);
+    console.log("called with", args, ", returned", result);
+    return result;
+  };
+}
+noisy(Math.min)(3, 2, 1);
+// → calling with [3, 2, 1]
+// → called with [3, 2, 1] , returned 1
