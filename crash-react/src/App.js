57fffd3478085d7c9faec86173f0ffe8d370b93e
starydv7 <pawanstar72926@gmail.com>
2022-10-10 01:58:02 +0530

    addcontact app

--- crash-react/src/App.js
+++ crash-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AddContact from './components/AddContact';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+      <Header />
+      <AddContact/>
     </div>
   );
 }
