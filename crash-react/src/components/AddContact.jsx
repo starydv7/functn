65d0be877e1b491de601c3e8784854252bf65523
starydv7 <pawanstar72926@gmail.com>
2022-10-10 22:46:37 +0530

    form

--- crash-react/src/components/AddContact.jsx
+++ crash-react/src/components/AddContact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from "./addcontact.module.css";
 const AddContact = () => {
+
+  
   return (
     <div className={styles.form - 1}>
       <div className="form-layout">
