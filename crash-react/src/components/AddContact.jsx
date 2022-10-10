2863ccca976f6aaa23652d2e38c4422762a7d44b
starydv7 <pawanstar72926@gmail.com>
2022-10-10 23:52:30 +0530

    contact list

--- crash-react/src/components/AddContact.jsx
+++ crash-react/src/components/AddContact.jsx
@@ -1,17 +1,70 @@
-import React from 'react'
+import React from "react";
+
 import styles from "./addcontact.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
 const AddContact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  
+  const [contact, setContact]=useState("");
+  const [message, setMessage] = useState("");
+  const [role, setRole] = useState("");
+  const [github, setGithub] = useState("");
+  const [url, setUrl] = useState("");
+  const [linkdin, setLinkdin] = useState("");
+
+   let handleSubmit = async (e) => {
+     e.preventDefault();
+     try {
+       let res = await fetch("https://apipawan3.herokuapp.com/users", {
+         method: "POST",
+         body: JSON.stringify({
+           name: name,
+           email: email,
+           contact:contact,
+           role: role,
+           url: url,
+           github: github,
+           linkdin:linkdin,
+          
+         }),
+       });
+       let resJson = await res.json();
+       if (res.status === 200) {
+         setName("");
+         setEmail("");
+         setContact("");
+         setGithub('');
+         setRole("");
+         setUrl("");
+         setLinkdin("");
+         setMessage("User created successfully");
+       } else {
+         setMessage("Some error occured");
+       }
+     } catch (err) {
+       console.log(err);
+     }
+   };
+
 
 
   return (
     <div className={styles.form - 1}>
       <div className="form-layout">
         <h3> Add Details Here</h3>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="form-div">
             <label>Name</label>
             <br></br>
-            <input type="text" placeholder="Name" className={styles.name} />
+            <input
+              type="text"
+              placeholder="Name"
+              className={styles.name}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <br></br>
           <div className="form-div2">
@@ -19,30 +72,46 @@ const AddContact = () => {
             <br></br>
             <input
               type="text"
+              value={contact}
               placeholder="Contact Number"
               className={styles.contact}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <br></br>
           <div className="form-div2">
             <label>Email</label>
             <br></br>
-            <input type="text" placeholder="Email" className={styles.email} />
+            <input
+              type="text"
+              value={email}
+              placeholder="Email"
+              className={styles.email}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="form-div2">
             <label>Linkedin id</label>
             <br></br>
             <input
               type="url"
+              value={url}
               placeholder="Linkdin Url"
               className={styles.url}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <br></br>
           <div className="form-div2">
             <label>Github Account</label>
             <br></br>
-            <input type="url" placeholder="Github Url" className={styles.url} />
+            <input
+              type="url"
+              placeholder="Github Url"
+              value={github}
+              className={styles.url}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <br></br>
           <div className="form-div2">
@@ -52,6 +121,8 @@ const AddContact = () => {
               type="text"
               placeholder="Frontend/Backend-Dev"
               className={styles.url}
+              value={role}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <input type="submit" className={styles.btn} />
