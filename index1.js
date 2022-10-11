7d20cd69a91a53b6149f6f0add5caea58c99c9eb
starydv7 <pawanstar72926@gmail.com>
2022-10-11 22:10:18 +0530

    sum

--- index1.js
+++ index1.js
@@ -11,7 +11,6 @@ function equilibrium(arr, n)
              leftsum = 0;
               for(let j = 0; j < i; j++)
               leftsum += arr[j];
-               
               rightsum = 0;
               for(let j = i + 1; j < n; j++)
               rightsum += arr[j];   
