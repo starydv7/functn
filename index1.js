0280bc2c4e690ff8ffbe3f4676664ef9035d1bcd
starydv7 <pawanstar72926@gmail.com>
2022-10-11 19:26:40 +0530

    equillbrium sum

---
+++ index1.js
@@ -0,0 +1,30 @@
+function equilibrium(arr, n)
+{
+         var i, j;
+         var leftsum, rightsum;
+          
+         
+         for(i = 0; i < n; ++i)
+         {
+          
+           
+             leftsum = 0;
+              for(let j = 0; j < i; j++)
+              leftsum += arr[j];
+               
+              
+              rightsum = 0;
+              for(let j = i + 1; j < n; j++)
+              rightsum += arr[j];
+               
+             
+              if(leftsum == rightsum)
+                 return i;
+         }
+          
+        
+            return -1;
+}
+ var arr = new Array(-7,1,5,2,-4,3,0);
+n = arr.length;
+console.log(equilibrium(arr, n));
