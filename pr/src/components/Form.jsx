7497e5011e77e0c1d307d99d2504a5653ba2c47e
starydv7 <pawanstar72926@gmail.com>
2022-10-11 22:42:07 +0530

    form

--- pr/src/components/Form.jsx
+++ pr/src/components/Form.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import Pagination from "react-js-pagination";
 
 const User = () => {
     const [data, setData] = useState([]);
@@ -105,65 +106,110 @@ const User = () => {
     //   function Editdata(id) {
     //     localStorage.setItem("newid", id);
     //   }
-
+    // const handlePageChange = (pageNumber) => {
+    //       console.log(`active page is ${pageNumber}`);
+    //       axios
+    //         .get(
+    //           `https://apipawan3.herokuapp.com/users?_page=${pageNumber}&_limit=20`
+    //         )
+    //         .then((res) => {
+    //           this.setState({
+    //             data: res.data,
+    //           });
+    //         });
+    //       this.setState({ activePage: pageNumber });
+    // }
+//      handlePageChange = pageNumber => {
+//     console.log(`active page is ${pageNumber}`);
+//     axios
+//       .get(
+//         `https://apipawan3.herokuapp.com/users?_page=${pageNumber}&_limit=20`
+//       )
+//       .then(res => {
+//         this.setState({
+//           data: res.data
+//         });
+//       });
+//     this.setState({ activePage: pageNumber });
+//   };
+//   render() {
+//     const allData = this.state.data.map(item => {
+//       return <li key={item.id}>{item.mobileNumber}
+//       </li>
+//     });
     return (
-        <div className="App">
-            <form onSubmit={handleSubmit}>
-                <select value={category} onChange={(e) => setCategory(e.target.value)}>
-                    <option value="/">Category</option>
-                    <option value="deluxe">Deluxe</option>
-                    <option value="family">Family</option>
-                    <option value="suite">Suite</option>
-                </select>
-                <select
-                    value={typerooms}
-                    onChange={(e) => setTypeRooms(e.target.value)}
-                >
-                    <option value="/">Type of Rooms</option>
-                    <option value="ac">AC</option>
-                    <option value="non-ac">Non-Ac</option>
-                </select>
-                <select value={bedtypes} onChange={(e) => setBedTypes(e.target.value)}>
-                    <option value="/">Type of Bed</option>
-                    <option value="double">Double</option>
-                    <option value="single">Single</option>
-                </select>
-                <input
-                    type="text"
-                    value={adults}
-                    placeholder="Adults"
-                    onChange={(e) => setAdults(e.target.value)}
-                />
-                <input
-                    type="text"
-                    value={capacity}
-                    placeholder="Capacity"
-                    onChange={(e) => setCapacity(e.target.value)}
-                />
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="/">Category</option>
+            <option value="deluxe">Deluxe</option>
+            <option value="family">Family</option>
+            <option value="suite">Suite</option>
+          </select>
+          <select
+            value={typerooms}
+            onChange={(e) => setTypeRooms(e.target.value)}
+          >
+            <option value="/">Type of Rooms</option>
+            <option value="ac">AC</option>
+            <option value="non-ac">Non-Ac</option>
+          </select>
+          <select
+            value={bedtypes}
+            onChange={(e) => setBedTypes(e.target.value)}
+          >
+            <option value="/">Type of Bed</option>
+            <option value="double">Double</option>
+            <option value="single">Single</option>
+          </select>
+          <input
+            type="text"
+            value={adults}
+            placeholder="Adults"
+            onChange={(e) => setAdults(e.target.value)}
+          />
+          <input
+            type="text"
+            value={capacity}
+            placeholder="Capacity"
+            onChange={(e) => setCapacity(e.target.value)}
+          />
 
-                <input
-                    type="text"
-                    value={cost}
-                    placeholder="Price"
-                    onChange={(e) => setCost(e.target.value)}
-                />
-                <input
-                    type="url"
-                    placeholder="img url"
-                    value={img_url}
-                    onChange={(e) => setImg_Url(e.target.value)}
-                />
-                <input
-                    type="checked"
-                    value={booked}
-                    onChange={(e) => setBooked(e.target.value)}
-                />
+          <input
+            type="text"
+            value={cost}
+            placeholder="Price"
+            onChange={(e) => setCost(e.target.value)}
+          />
+          <input
+            type="url"
+            placeholder="img url"
+            value={img_url}
+            onChange={(e) => setImg_Url(e.target.value)}
+          />
+          <input
+            type="checked"
+            value={booked}
+            onChange={(e) => setBooked(e.target.value)}
+          />
 
-                <button type="submit">Submit</button>
+          <button type="submit">Submit</button>
 
-                <div className="message">{message ? <p>{message}</p> : null}</div>
-            </form>
-        </div>
-    )
+          <div className="message">{message ? <p>{message}</p> : null}</div>
+        </form>
+        <p>React pagination test</p>
+        <ul>{allData}</ul>
+        <Pagination
+          totalItemsCount={450}
+          onChange={handlePageChange}
+          activePage={this.state.activePage}
+          itemsCountPerPage={10}
+          pageRangeDisplayed={5}
+        />
+      </div>
+    );
 }
 export default User;
