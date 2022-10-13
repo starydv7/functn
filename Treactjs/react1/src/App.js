e507210322d9db34549170c7e301094098946c01
starydv7 <pawanstar72926@gmail.com>
2022-10-13 23:19:24 +0530

    data delete

--- Treactjs/react1/src/App.js
+++ Treactjs/react1/src/App.js
@@ -8,7 +8,7 @@ function App() {
       id:2,userName:"yadav",age:23
     }
   ])
-  const handleDelete = () => {
+  const handleDelete = (id) => {
     const newData = data.filter(data => data.id !== id)
     setData(newData);
   }
