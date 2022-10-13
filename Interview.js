9ddede2040cde399381cd48e1baef06a1bc2a58c
starydv7 <pawanstar72926@gmail.com>
2022-10-14 00:54:50 +0530

    let and var

--- Interview.js
+++ Interview.js
@@ -16,6 +16,6 @@
 //because var is global scope and setTimeout takes some to show then 3 times loops run
 //when we do same things with LET it will print counting because LET is blocked scope only
 
-for (let i = 0; i < 3; i++){
-    setTimeout(() => console.log(i), 1);
-}
+// for (let i = 0; i < 3; i++){
+//     setTimeout(() => console.log(i), 1);
+// }
