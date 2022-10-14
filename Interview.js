d4c70dfd0b94d2869a8c407e5a9ad142871056af
starydv7 <pawanstar72926@gmail.com>
2022-10-14 15:32:49 +0530

    interview

--- Interview.js
+++ Interview.js
@@ -40,3 +40,13 @@
 // console.log(
 //     c === d
 // );
+
+const Person=({
+    name: "Pawan",
+    age: "24",
+    printInfo: function () {
+        console.log(`My name is ${this.name} and my age is ${this.age}`);
+        }
+            
+})
+Person.printInfo();
