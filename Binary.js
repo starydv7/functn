9a535f11a2273606754d0a5aebf822e874c1aab2
starydv7 <pawanstar72926@gmail.com>
2022-10-16 16:06:11 +0530

    map

--- Binary.js
+++ Binary.js
@@ -5,9 +5,9 @@ function binary(x) {
 const outputmap = arr.map(binary);
 console.log(outputmap);
 
-const arr = [1, 3, 5, 6, 8];
+const arrFilter = [1, 3, 5, 6, 8];
 function isEven(x) {
     return x % 2 === 0;
 }
-var output = arr.filter(isEven);
+var output = arrFilter.filter(isEven);
 console.log(output);
