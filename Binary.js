656abc8affbe103cbda87f2fc25cd5d85833d173
starydv7 <pawanstar72926@gmail.com>
2022-10-16 16:07:21 +0530

    even

--- Binary.js
+++ Binary.js
@@ -7,7 +7,8 @@ console.log(outputmap);
 
 const arrFilter = [1, 3, 5, 6, 8];
 function isEven(x) {
-    return x % 2 === 0;
+    // return x % 2 === 0;
+    return x > 4;
 }
 var output = arrFilter.filter(isEven);
 console.log(output);
