0fde9a7894615bb65d2adf1fda6ae05e1dbea3b6
starydv7 <pawanstar72926@gmail.com>
2022-10-21 00:52:46 +0530

    useeffect

--- interview/src/App.js
+++ interview/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from "./components/Home";
+import UseEffect from './components/UseEffect';
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <Home />
+      <UseEffect/>
     </div>
   );
 }
