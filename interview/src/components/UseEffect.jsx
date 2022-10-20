1cc3783e98437f82b97c6ee26842b48bac0b1f56
starydv7 <pawanstar72926@gmail.com>
2022-10-21 01:03:09 +0530

    useEffect

--- interview/src/components/UseEffect.jsx
+++ interview/src/components/UseEffect.jsx
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react';
 import axios from "axios";
 const UseEffect = () => {
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/comments").then((response) => {
-            console.log(response);
+        axios.get("https://jsonplaceholder.typicode.com/comments")
+            .then((response) => {
+            console.log(response.data);
          });
     })
   return (
