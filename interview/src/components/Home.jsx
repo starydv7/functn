06b9fa5ffd277925d11c4ec784173bd3efaaab90
starydv7 <pawanstar72926@gmail.com>
2022-10-20 23:12:04 +0530

    state

--- interview/src/components/Home.jsx
+++ interview/src/components/Home.jsx
@@ -3,7 +3,7 @@ const reducer = (state, action) => {
     switch (action.type) {
         case "INCREMENT":
             return {
-                count: state.count + 1, showText: state.showText
+            count: state.count + 1, showText: state.showText
             }
         case "toggleShowText":
             return { count: state.count, showText: !state.showText };
