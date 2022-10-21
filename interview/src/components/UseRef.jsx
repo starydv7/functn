5b0e8afc645ce7445289102a3ebe5da9aad5f3e9
starydv7 <pawanstar72926@gmail.com>
2022-10-21 15:03:57 +0530

    useRef

--- interview/src/components/UseRef.jsx
+++ interview/src/components/UseRef.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useRef} from 'react'
 
 const UseRef = () => {
+    const inputRef = useRef(null);
+
+    const onClick = () => {
+        inputRef.current.focus();
+    }
   return (
-    <div>UseRef</div>
+      <div>UseRef
+          <h1>Pawan</h1>
+          <input type="text" placeholder="Enter your input..." ref={inputRef} />
+          <button onClick={onClick }>Chnage the current</button>
+    </div>
   )
 }
 
