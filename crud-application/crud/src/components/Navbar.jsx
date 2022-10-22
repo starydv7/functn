d24187bc3f44b53aa4a1ca49dc30e2a314e50be7
starydv7 <pawanstar72926@gmail.com>
2022-10-23 00:27:14 +0530

    post

---
+++ crud-application/crud/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+  return (
+      <div>
+          <Link to="/">Home</Link>
+          <Link to="/create">Create List</Link>
+          <Link to="/update">Update</Link>
+    </div>
+  )
+}
+
+export default Navbar
