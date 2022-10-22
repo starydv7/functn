4e293780cd942bcd921e7936c2eef610a8a7a23f
starydv7 <pawanstar72926@gmail.com>
2022-10-23 00:37:45 +0530

    crud

--- crud-application/crud/src/App.js
+++ crud-application/crud/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import AllRoutes from './components/AllRoutes';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <AllRoutes/>
     </div>
   );
--- crud-application/crud/src/components/AllRoutes.jsx
+++ crud-application/crud/src/components/AllRoutes.jsx
@@ -3,13 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Create from './Create';
 import Home from './Home';
 import Main from './Main';
+import Update from './Update';
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create/>} />
-        <Route path="/" element={<Main/>}/>
+        <Route path="/create" element={<Create />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/update" element={<Update/>} />
       </Routes>
     </div>
   );
