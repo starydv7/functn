3cab838a56bb422282fd7cc5393290a7e4d1818e
starydv7 <pawanstar72926@gmail.com>
2022-10-23 00:51:00 +0530

    crud

--- crud-application/crud/src/components/Create.jsx
+++ crud-application/crud/src/components/Create.jsx
@@ -34,17 +34,16 @@ const Create = () => {
     }
   return (
     <div>
-      <form class="w-full max-w-lg mx-auto my-20" onSubmit={handleSubmit}>
-        <div class="flex flex-wrap mx-3 mb-2">
-          <div class="w-full md:w-1/2 px-3 mb-3 md:mb-0">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-first-name"
+             
             >
               First Name
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+             
               id="grid-first-name"
               type="text"
               placeholder="First Name"
