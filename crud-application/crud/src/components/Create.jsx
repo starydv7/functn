a9e81dbb17f0f5d647e2aea75c15432d750bd8f6
starydv7 <pawanstar72926@gmail.com>
2022-10-23 00:38:33 +0530

    post

--- crud-application/crud/src/components/Create.jsx
+++ crud-application/crud/src/components/Create.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Create = () => {
+     const [fname, setFname] = useState("");
+     const [lname, setLname] = useState("");
+     const [username, setUsername] = useState("");
+     const [email, setEmail] = useState("");
+     const [avatar, setAvatar] = useState("");
   return (
     <div>Create</div>
   )
