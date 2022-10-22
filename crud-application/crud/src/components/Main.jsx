d9c0df16d9b5a70799b937199022e5728894a17a
starydv7 <pawanstar72926@gmail.com>
2022-10-22 23:46:42 +0530

    post

--- crud-application/crud/src/components/Main.jsx
+++ crud-application/crud/src/components/Main.jsx
@@ -1,39 +1,74 @@
 import React from 'react';
-import axios from "axios";
-import { useHistory } from "react-router";
+import { useEffect } from 'react';
 import { useState } from 'react';
+import { Link } from "react-router-dom";
 
 const Main = () => {
-    let history = useHistory();
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [checkbox, setCheckbox] = useState(false);
-    console.log(checkbox);
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        
+    }, []);
 
-    const postData = () => {
-        axios.post(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData`, {
-            firstName,
-            lastName,
-            checkbox
-        }).then(() => {
-            history.push('/read')
-        })
-    }
-  return (
-      <div>
-          <form>
-              <label>First Name</label>
-              <input placeholder='firstName' onChange={(e) => setFirstName(e.target.value)} />
-              
-              <label>Last Name</label>
-              <input placeholder='lastName' onChange={(e) => setLastName(e.target.value)} />
-              
-              <input type="checkbox" label="I agree tc" onChange={(e) => setCheckbox(!checkbox)} />
-              
-              <button onClick={postData} >Submit</button>
-          </form>
-    </div>
-  )
+    const fetchData = () => {
+        fetch("https://www.mecallapi.com/api/users")
+            .then((res) => res.json())
+            .then((result) => {
+                setUsers(result)
+            })
+    };
+    return (
+      <>
+        <div>
+          <div>
+            {" "}
+            <h3>Users List</h3>
+          </div>
+          <div>
+            <span>
+              <Link to="/main">
+                <button type="button">Create</button>
+              </Link>
+            </span>
+          </div>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Full Name</th>
+                <th>Email Address</th>
+                <th>Edit</th>
+                <th>DELETE</th>
+              </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <tr key={user.ID}>
+                                <td>
+                                    <div>
+                                        {user.id}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        <img src={user.avatar} alt="avatar"/>
+                                    </div>
+                                    <div>
+                                        <span>{user.fname}<span>{user.lname}</span></span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <button>EDIT</button>
+                                    <button>Delete</button>
+                                </td>
+                           </tr>
+                       ))}
+                    </tbody>
+          </table>
+        </div>
+      </>
+    );
 }
 
-export default Main
+export default Main;
