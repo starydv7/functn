87b07c9ff5e18c299eb55b8d6f63a72f8658700a
starydv7 <pawanstar72926@gmail.com>
2022-10-23 00:27:38 +0530

    all

--- crud-application/crud/src/App.js
+++ crud-application/crud/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Main from './components/Main';
 import { Router,Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <Main/>
     </div>
   );
