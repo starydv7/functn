4fe72c5bfeab430d6966554ce27b1d6a6343ad2e
starydv7 <pawanstar72926@gmail.com>
2022-10-23 21:36:11 +0530

    update

--- crud-application/crud/src/components/Update.jsx
+++ crud-application/crud/src/components/Update.jsx
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
-import useParams from "react-router-dom";
+import { useParams } from 'react-router';
+
 const Update = () => {
   const { id } = useParams();
   const [fname, setFname] = useState("");
@@ -8,10 +9,10 @@ const Update = () => {
   const [email, setEmail] = useState("");
   const [avatar, setAvatar] = useState("");
   useEffect(() => {
-    fetch("https://www.mecallapi.com/api/users/+id")
+    fetch(`https://www.mecallapi.com/api/users/`)
       .then((res) => res.json())
       .then((result) => {
-        setFname(result.user.fname)
+        setFname(result.user.fname);
         setLname(result.user.lname);
         setUsername(result.user.username);
         setEmail(result.user.email);
@@ -29,36 +30,34 @@ const Update = () => {
       "email": email,
       "avatar":avatar,
     }
-    fetch("https://www.mecallapi.com/api/users/update", {
+    fetch(`https://www.mecallapi.com/api/users/${id}`, {
       method: "PUT",
       headers: {
         Accept: "application/form-data",
-        "Content-Type":"application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-      
     })
       .then((res) => res.json())
       .then((result) => {
-        alert(result["message"])
-        if (result["status"] === 'ok') {
+        alert(result["message"]);
+        if (result["status"] === "ok") {
           window.location.href = "/";
         }
-    })
+      });
   }
   return (
     <div>
-      <form class="w-full max-w-lg mx-auto my-20" onSubmit={handleSubmit}>
-        <div class="flex flex-wrap mx-3 mb-2">
-          <div class="w-full md:w-1/2 px-3 mb-3 md:mb-0">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-first-name"
+             
             >
               First Name
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+             
               id="grid-first-name"
               type="text"
               value={fname}
@@ -66,15 +65,14 @@ const Update = () => {
               label="First Name"
             />
           </div>
-          <div class="w-full md:w-1/2 px-3">
+          <div>
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-last-name"
+            
             >
               Last Name
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            
               id="grid-last-name"
               type="text"
               value={lname}
@@ -83,16 +81,15 @@ const Update = () => {
             />
           </div>
         </div>
-        <div class="flex flex-wrap mx-3 mb-2">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <div>
+          <div>
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-first-name"
+             
             >
               Email Address
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+             
               id="grid-first-name"
               type="email"
               value={email}
@@ -100,15 +97,14 @@ const Update = () => {
               label="Email"
             />
           </div>
-          <div class="w-full md:w-1/2 px-3">
+          <div>
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-last-name"
+             
             >
               Username
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            
               id="grid-last-name"
               label="Username"
               onChange={(e) => setUsername(e.target.value)}
@@ -117,17 +113,15 @@ const Update = () => {
             />
           </div>
         </div>
-        <div class="flex flex-wrap mx-3 mb-1">
-          <div class="w-full px-3">
+        <div>
+          <div>
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              for="grid-image"
+              
             >
               Avatar
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-image"
+             
               type="text"
               value={avatar}
               label="Avatar"
@@ -137,7 +131,7 @@ const Update = () => {
         </div>
         <button
           type="submit"
-          class="inline-flex items-center ml-8 px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+         
         >
           Update
         </button>
