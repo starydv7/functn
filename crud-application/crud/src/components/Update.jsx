ebcc2745e944bd631dc0ad563457229d3e934b04
starydv7 <pawanstar72926@gmail.com>
2022-10-23 19:54:42 +0530

    post data

--- crud-application/crud/src/components/Update.jsx
+++ crud-application/crud/src/components/Update.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-
-const Update = () => {
+import React,{useState,useEffect} from 'react'
+import useParams from "react-router-dom";
+const Update = (id) => {
+  const { id } = useParams();
   return (
     <div>Update</div>
   )
