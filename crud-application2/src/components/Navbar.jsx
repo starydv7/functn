e7017cbca8db06ab69f4b2e63b7d367b1b4b54d3
starydv7 <pawanstar72926@gmail.com>
2022-10-26 23:50:09 +0530

    hags

--- crud-application2/src/components/Navbar.jsx
+++ crud-application2/src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useEffect } from 'react';
 
 const Navbar = () => {
+
+  const [data, setData] = useState(0);
+  const handleClick = () => {
+    setData(data + 1);
+  }
+  useEffect(() => {
+    console.log(setData)
+  });
+  //  console.log(data);
   return (
-    <div></div>
+    <div>
+      {data}
+      {/* <h2></h2> */}
+      <button onClick={handleClick}>{data}</button>
+      
+    </div>
   )
 }
 export default Navbar;
