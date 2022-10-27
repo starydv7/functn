21c317c339dfdd29e93149e1f15eebfd580b37b9
starydv7 <pawanstar72926@gmail.com>
2022-10-28 00:50:16 +0530

    api

--- crud-application2/src/components/Validation.jsx
+++ crud-application2/src/components/Validation.jsx
@@ -2,26 +2,55 @@ import React, { useState } from 'react';
 
 
 const Validation = () => {
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
-    const handleChange = (e) => {
-        e.preventDefault();
-    }
-    function userHandler(e) {
-        let item = e.target.value;
-        if (item.length < 3) {
-            console.log("Invalid");
-        }
-        console.log(e.target.value.length)
-    }
+     const [user, setUser] = useState("");
+     const [password, setPassword] = useState("");
+     const [userErr, setUserErr] = useState(false);
+    const [passErr, setPassErr] = useState(false);
+     function loginHandle(e) {
+       if (user.length < 3 || password.length < 3) {
+         alert("type correct values");
+       } else {
+         alert("all good :)");
+       }
+
+       e.preventDefault();
+     }
+     function userHandler(e) {
+       let item = e.target.value;
+       if (item.length < 3) {
+         setUserErr(true);
+       } else {
+         setUserErr(false);
+       }
+       setUser(item);
+     }
+     function passwordHandler(e) {
+       let item = e.target.value;
+       if (item.length < 3) {
+         setPassErr(true);
+       } else {
+         setPassErr(false);
+       }
+       setPassword(item);
+     }
   return (
-      <div>
-          <form onSubmit={handleChange}>
-              <input type='text' placeholder="name"onChange={userHandler} />
-              <input type="text"placeholder="password" onChange={userHandler}/>
-          </form>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={loginHandle}>
+        <input type="text" placeholder="Enter User Id" onChange={userHandler} />
+        {userErr ? <span>User Not Valid</span> : ""}
+        <br /> <br />
+        <input
+          type="password"
+          placeholder="Enter User Password"
+          onChange={passwordHandler}
+        />
+        {passErr ? <span>Password Not Valid</span> : ""}
+        <br /> <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Validation
