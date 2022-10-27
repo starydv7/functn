b555b57d28ec44171740de6afe22d7a61694855a
starydv7 <pawanstar72926@gmail.com>
2022-10-27 19:23:00 +0530

    form validation

--- crud-application2/src/components/FormValidation.jsx
+++ crud-application2/src/components/FormValidation.jsx
@@ -7,10 +7,9 @@ const FormValidation = () => {
         <input type="text" placeholder="First Name" />
         <input type="text" placeholder="Last Name" />
         <input type="number" placeholder="Mobile Number" />
-        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="user id" />
       </form>
     </div>
   );
 }
-
-export default FormValidation
+export default FormValidation;
