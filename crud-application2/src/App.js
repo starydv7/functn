60bbd3a23c9c6bde449c262aab972552806034e5
starydv7 <pawanstar72926@gmail.com>
2022-10-27 20:02:04 +0530

    form

--- crud-application2/src/App.js
+++ crud-application2/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import FormValidation from './components/FormValidation';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+     <FormValidation/>
     </div>
   );
 }
--- crud-application2/src/components/FormValidation.jsx
+++ crud-application2/src/components/FormValidation.jsx
@@ -13,31 +13,45 @@ const FormValidation = () => {
     <div>
       <form>
         <label>First Name</label>
+        <br></br>
         <input
           type="text"
           placeholder="First Name"
           value={firstName}
-          onChange={(e)=>{setFirstName(e.target.value)}}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
         />
+        <br></br>
         <label>Last Name</label>
+        <br></br>
         <input
           type="text"
           placeholder="Last Name"
           value={lastName}
-          onChange={(e)=>{setLastName(e.target.value)}}
-
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
         />
+        <br></br>
+        <label>Mobile Number</label>
+        <br></br>
         <input
           type="number"
           placeholder="Mobile Number"
           value={mobileNumber}
-          onChange={(e)=>setMobileNumber(e.target.value)}
+          onChange={(e) => setMobileNumber(e.target.value)}
         />
+        <br></br>
+        <label>User ID</label>
+        <br></br>
         <input
           type="text"
           placeholder="user id"
           value={userId}
-          onChange={(e)=>{setUserId(e.target.value)}}
+          onChange={(e) => {
+            setUserId(e.target.value);
+          }}
         />
       </form>
     </div>
