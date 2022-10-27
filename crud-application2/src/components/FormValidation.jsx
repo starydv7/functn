f4409b6c9888e0d6ed05e207e567ec375a16623b
starydv7 <pawanstar72926@gmail.com>
2022-10-27 19:43:13 +0530

    form

--- crud-application2/src/components/FormValidation.jsx
+++ crud-application2/src/components/FormValidation.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useState } from 'react';
 
 const FormValidation = () => {
+  const [FirstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
     <div>
       <form>
-        <input type="text" placeholder="First Name" />
+        <input
+          type="text"
+          placeholder="First Name"
+        />
         <input type="text" placeholder="Last Name" />
         <input type="number" placeholder="Mobile Number" />
         <input type="text" placeholder="user id" />
