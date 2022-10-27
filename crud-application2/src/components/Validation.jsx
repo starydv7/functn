dc6abebbc000a317958aca3c4b3b497a1c0252ff
starydv7 <pawanstar72926@gmail.com>
2022-10-28 00:15:34 +0530

    nuw

--- crud-application2/src/components/Validation.jsx
+++ crud-application2/src/components/Validation.jsx
@@ -4,11 +4,21 @@ import React, { useState } from 'react';
 const Validation = () => {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
+    const handleChange = (e) => {
+        e.preventDefault();
+    }
+    function userHandler(e) {
+        let item = e.target.value;
+        if (item.length < 3) {
+            console.log("Invalid");
+        }
+        console.log(e.target.value.length)
+    }
   return (
       <div>
-          <form>
-              <input type='text' placeholder="name" />
-              <input type="text"placeholder="password"/>
+          <form onSubmit={handleChange}>
+              <input type='text' placeholder="name"onChange={userHandler} />
+              <input type="text"placeholder="password" onChange={userHandler}/>
           </form>
     </div>
   )
