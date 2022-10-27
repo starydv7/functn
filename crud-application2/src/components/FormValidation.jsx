e23514171d43758f1b7b252fdf8554305c538c1a
starydv7 <pawanstar72926@gmail.com>
2022-10-27 14:17:15 +0530

    promise

---
+++ crud-application2/src/components/FormValidation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FormValidation = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="number" placeholder="Mobile Number" />
+        <input type="text" placeholder="First Name" />
+      </form>
+    </div>
+  );
+}
+
+export default FormValidation
