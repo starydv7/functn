bfd82cfe3ab42b9f2fcdbb240b9958499d891d08
starydv7 <pawanstar72926@gmail.com>
2022-10-28 12:54:20 +0530

    fetch

---
+++ crud-application2/src/components/FetchGet.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import React from "react";
+
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
+  return (
+      <div>
+          {post.map((el) => (
+              <div>{el.title}</div>
+        ))}
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+}
