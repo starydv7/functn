431be2930462c8901dd021c268a3447ee58f9f1d
starydv7 <pawanstar72926@gmail.com>
2022-10-28 15:32:37 +0530

    form post

--- crud-application2/src/components/FetchGet.jsx
+++ crud-application2/src/components/FetchGet.jsx
@@ -17,28 +17,64 @@ const FetchGet = () => {
                 })
         })
     }, [])
-    
+    const saveUser = () => {
+        let data={name,title,mobilenumber}
+        console.log(name, title, mobilenumber);
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "content-type":"aplication/json"
+            },
+            body: JSON.stringify(data)
+                
+            
+        }).then((result) => {
+            console.log("result", result);
+        })
+    }
   return (
     <div>
       <form>
-              <input
-                  type="text"
-                  value={name}
-                  placeholder="name"
-                  name="name" />
+        <input
+          type="text"
+          value={name}
+          placeholder="name"
+          name="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
         <br />
-              <input
-                  type="text"
-                  placeholder="title"
-                  name="title" />
-              <input
-                  type="text"
-                  placeholder="mobilenumber"
-                  name="mobilenumber" />
+        <input
+          type="text"
+          value={title}
+          placeholder="title"
+          name="title"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          value={mobilenumber}
+          placeholder="mobilenumber"
+          name="mobilenumber"
+          onChange={(e) => {
+            setMobileNumber(e.target.value);
+          }}
+        />
+        <button
+          type="button"
+          onClick={saveUser}
+          style={{ backgroundColor: "red", color: "white" }}
+        >
+          Sava Data to Api
+        </button>
         <br />
         <br />
       </form>
-      {data.length > 0 &&
+      {/* {data.length > 0 &&
         data.map((item) => {
           return (
             <div key={item.id}>
@@ -47,7 +83,7 @@ const FetchGet = () => {
               <span></span>
             </div>
           );
-        })}
+        })} */}
     </div>
   );
 }
