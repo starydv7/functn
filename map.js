c2142b8f61711ef621efce2d13089b3d163bc0c1
starydv7 <pawanstar72926@gmail.com>
2022-11-03 15:07:00 +0530

    map function

--- map.js
+++ map.js
@@ -1,4 +1,8 @@
 var arr = [1, 2, 4, 6, 70];
-arr.map((el)=>{
+arr.map((el) => {
     console.log(el * 2);
-})
+});
+//Set method 
+var str = [1, 4, 6, 7, 9, 3, 2, 5, 7, 5, 4, 3, 2, 5, 22, 2, 2, 2, 4444, 4, 4, 4,];
+let uniqueChar = [... new Set(str)];
+console.log(uniqueChar);
