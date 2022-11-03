fbfcc5d1f57946f28d97feaa36dc7ea1f39e47c5
starydv7 <pawanstar72926@gmail.com>
2022-11-03 15:43:06 +0530

    set method

--- map.js
+++ map.js
@@ -6,3 +6,38 @@ arr.map((el) => {
 var str = [1, 4, 6, 7, 9, 3, 2, 5, 7, 5, 4, 3, 2, 5, 22, 2, 2, 2, 4444, 4, 4, 4,];
 let uniqueChar = [... new Set(str)];
 console.log(uniqueChar);
+
+//highest number of occurance
+// var arr = [1, 2, 4, 1, 1, 5];
+// var obj = {};
+// for (var i in arr) {
+//     if (arr[i] == arr[i + 1]) {
+//         obj++;
+//     }
+//     console.log(arr[obj]);
+// }
+var array = [1, 2, 3, 4, 5, 1, 3, 4, 1];
+function mode(array)
+{
+    if(array.length == 0)
+        return null;
+    var modeMap = {};
+    var maxEl = array[0], maxCount = 1;
+    for(var i = 0; i < array.length; i++)
+    {
+        var el = array[i];
+        if(modeMap[el] == null)
+            modeMap[el] = 1;
+        else
+            modeMap[el]++;  
+        if(modeMap[el] > maxCount)
+        {
+            maxEl = el;
+            maxCount = modeMap[el];
+        }
+    }
+    return maxEl;
+    return el;
+}
+var res = mode(array);
+console.log(res);
